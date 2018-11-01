653eec421a9f9e39b52ba8e299564be567713723
Evan <ries8898@gmail.com>
2018-11-01 15:26:19 +0300

    Initial commit

---
+++ Hello_world/Hello_world/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Hello_world
+//
+//  Created by Иван on 31/10/2018.
+//  Copyright © 2018 Иван. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var i=5
+print(i+17)
+
